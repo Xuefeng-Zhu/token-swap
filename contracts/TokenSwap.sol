488bed604975ad7bfad65900ccadb18da2cf741c
Xuefeng-Zhu <xzhu15@illinois.edu>
2022-05-29 12:47:54 -0500

    update

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -3,27 +3,31 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenSwap {
+contract TokenSwap is Ownable {
     ERC20 public immutable token;
-    mapping(address => uint) public allocations;
-    mapping(address => uint) public claimed;
-    uint public start;
-    uint public vestingPeriod = 15552000; // 180 days
+    mapping(address => uint256) public allocations;
+    mapping(address => uint256) public claimed;
+    uint256 public start;
+    uint256 public vestingPeriod = 15552000; // 180 days
     bool public started;
-    address private creator;
 
-    constructor(address[] memory _users, uint[] memory _allocations, address _token) {
-        require(_users.length == _allocations.length, "!Length");
-        for (uint i=0; i<_users.length; i++) {
-            allocations[_users[i]] = _allocations[i];
-        }
-        creator = msg.sender;
+    constructor(address _token) {
         token = ERC20(_token);
     }
 
-    function startSwap() external {
-        require(msg.sender == creator, "!Creator");
+    function setAllocations(
+        address[] memory _users,
+        uint256[] memory _allocations
+    ) external onlyOwner {
+        require(_users.length == _allocations.length, "!Length");
+        for (uint256 i = 0; i < _users.length; i++) {
+            allocations[_users[i]] = _allocations[i];
+        }
+    }
+
+    function startSwap() external onlyOwner {
         require(!started, "Started");
         started = true;
         start = block.timestamp;
@@ -31,15 +35,19 @@ contract TokenSwap {
 
     function claim() external {
         require(started, "!Started");
-        uint _claimable = claimable(msg.sender);
+        uint256 _claimable = claimable(msg.sender);
         require(_claimable > 0, "Nothing to claim");
         claimed[msg.sender] += _claimable;
         token.transfer(msg.sender, _claimable);
     }
 
-    function claimable(address _user) public view returns (uint) {
+    function claimable(address _user) public view returns (uint256) {
         if (!started) return 0;
-        if (block.timestamp-start > vestingPeriod) return allocations[_user] - claimed[_user];
-        return allocations[_user]*(block.timestamp-start)/vestingPeriod - claimed[_user];
+        if (block.timestamp - start > vestingPeriod)
+            return allocations[_user] - claimed[_user];
+        return
+            (allocations[_user] * (block.timestamp - start)) /
+            vestingPeriod -
+            claimed[_user];
     }
 }
--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,11 +1,11 @@
+const csv = require('csvtojson');
+const { ethers } = require('hardhat');
+
 // Just a standard hardhat-deploy deployment definition file!
 const func = async (hre) => {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-
-  const users = ['0xa4d4a980c5cee392e0bcdeecdca29c8a4a24ac35'];
-  const allocations = ['1000000000000000000'];
 
   await deploy('MockERC20', {
     from: deployer,
@@ -13,13 +13,28 @@ const func = async (hre) => {
     log: true,
   });
 
-  const token = await deployments.get('MockERC20');
+  const token = await ethers.getContract('MockERC20', deployer);
 
   await deploy('TokenSwap', {
     from: deployer,
-    args: [users, allocations, token.address],
+    args: [token.address],
     log: true,
   });
+
+  const tokenSwap = await ethers.getContract('TokenSwap', deployer);
+  const balances = await csv().fromFile('./balances.csv');
+  for (let i = 0; i < balances.length; i += 100) {
+    console.log('setAllocations', i);
+    const curBalances = balances.slice(i, i + 100);
+    await tokenSwap.setAllocations(
+      curBalances.map((item) => item.HolderAddress),
+      curBalances.map((item) => item.Balance)
+    );
+  }
+
+  console.log('mint');
+  await token.mint(tokenSwap.address, '9999999999999999999999999');
+  await tokenSwap.startSwap();
 };
 
 func.tags = ['TokenSwap'];
